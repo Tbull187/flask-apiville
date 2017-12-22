d4c64d3f3f9f6665751b43cd84964067462797c2
Tristan Bull <tristan.tbull@gmail.com>
2017-12-21 21:50:16 -0800

    add templates for all views

--- app.py
+++ app.py
@@ -9,5 +9,32 @@ Scss(app, static_dir='static', asset_dir='assets')
 def index():
     return render_template('index.html')
 
+@app.route('/how-it-works')
+def howItWorks():
+    return render_template('how-it-works.html')
 
-app.run(debug=True, host='0.0.0.0', port=5000)
+@app.route('/products')
+def product():
+    return render_template('products.html')
+
+@app.route('/plans')
+def plans():
+    return render_template('plans.html')
+
+@app.route('/company')
+def company():
+    return render_template('company.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+@app.route('/sign-in')
+def signIn():
+    return render_template('sign-in.html')
+
+@app.route('/sign-up')
+def signUp():
+    return render_template('sign-up.html')
+
+# app.run(debug=True, host='0.0.0.0', port=5000)
